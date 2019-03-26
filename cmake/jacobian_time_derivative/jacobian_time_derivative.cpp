b04166465349b37d2bfb949367fb0f8280f3a450
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-03-26 16:47:34 +0900

    Added jacobian_time_derivative example

---
+++ cmake/jacobian_time_derivative/jacobian_time_derivative.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <dqrobotics/DQ.h>
+#include <dqrobotics/DQ_kinematics.h>
+#include <dqrobotics/robot_dh/Kukka.h>
+#include <cmath>
+
+using namespace DQ_robotics;
+int main()
+{
+
+    //Create a new DQ_kinematics object with KUKA LWR parameters
+    DQ_kinematics kuka = KukkaKinematics();
+
+    //Integration step for the numerical calculations
+    double T = 1e-3;
+    //Final time
+    double T_end = 2*M_PI;
+
+    VectorXd theta = VectorXd::Zero(7);
+
+    for(double t=0.0;t<T_end;t+=T)
+    {
+        //For simplicity, all joint trajectories are the same. All joints
+        //rotate at a frequency of T rad/s.
+        theta = sin(T*t)*VectorXd::Ones(7);
+        //This is the analytical time derivative of the joint trajectories.
+        VectorXd theta_dot = T*cos(t)*VectorXd::Ones(7);
+        //Calculation of the analytical Jacobian time derivative.
+        MatrixXd jacob_dot = kuka.jacobianDerivative(theta,theta_dot,7);
+        //First-order numerical approximation of the Jacobian time derivative
+        MatrixXd jacob_diff = (1.0/T)*(kuka.raw_jacobian(theta+theta_dot*T,7)-kuka.raw_jacobian(theta,7));
+
+        std::cout << "Coefficient with largest error = " << jacob_diff.maxCoeff() << std::endl;
+    }
+
+    return 0;
+}
--- cmake/jacobian_time_derivative/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <dqrobotics/DQ.h>
-#include <dqrobotics/DQ_kinematics.h>
-
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
-    return 0;
-}
