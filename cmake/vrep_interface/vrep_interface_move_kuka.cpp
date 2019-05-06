ae149c26a6885c850aaf83e5e8bbca97458e4c91
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-05-06 15:03:16 +0900

    Added vrep_interface example

---
+++ cmake/vrep_interface/vrep_interface_move_kuka.cpp
@@ -0,0 +1,85 @@
+/**
+(C) Copyright 2019 DQ Robotics Developers
+
+This file is part of DQ Robotics.
+
+    DQ Robotics is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DQ Robotics is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+- Murilo M. Marinho (murilo@nml.t.u-tokyo.ac.jp)
+*/
+
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <dqrobotics/DQ.h>
+#include <dqrobotics/robots/KukaLw4Robot.h>
+#include <dqrobotics/interfaces/vrep_interface.h>
+#include <dqrobotics/utils/DQ_LinearAlgebra.h>
+#include <dqrobotics/utils/DQ_Constants.h>
+
+int main(void)
+{
+    VrepInterface vi;
+
+    try
+    {
+        vi.connect(19997,100,10);
+
+        std::cout << "Starting V-REP simulation..." << std::endl;
+        vi.start_simulation();
+
+        std::vector<std::string> joint_names = {"LBR_iiwa_14_R820_joint1","LBR_iiwa_14_R820_joint2","LBR_iiwa_14_R820_joint3","LBR_iiwa_14_R820_joint4","LBR_iiwa_14_R820_joint5","LBR_iiwa_14_R820_joint6","LBR_iiwa_14_R820_joint7"};
+
+        std::vector<int> joint_handles = vi.get_object_handles(joint_names);
+
+        DQ_SerialManipulator robot = KukaLw4Robot::kinematics();
+
+        VectorXd theta_d(7);
+        theta_d << 0.,pi/2.,0.,pi/2.,0.,pi/2.,0.;
+        DQ xd = robot.fkm(theta_d);
+
+        VectorXd theta = vi.get_joint_positions(joint_handles,VrepInterface::OP_STREAMING);
+
+        VectorXd e(8);
+        e(0)=1.0;
+        std::cout << "Starting control loop..." << std::endl;
+        while(e.norm()>0.01)
+        {
+            theta      = vi.get_joint_positions(joint_handles,VrepInterface::OP_BUFFER);
+            DQ x       = robot.fkm(theta);
+            e          = vec8(x-xd);
+            MatrixXd J = robot.pose_jacobian(theta);
+
+            VectorXd u = -0.01*pinv(J)*e;
+            theta = theta+u;
+            vi.set_joint_target_positions(joint_handles,theta,VrepInterface::OP_ONESHOT);
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        std::cout << "Control finished..." << std::endl;
+
+        std::cout << "Stopping V-REP simulation..." << std::endl;
+        vi.stop_simulation();
+
+        vi.disconnect();
+
+    } catch (...)
+    {
+        std::cout << "There was an error connecting to V-REP, please check that it is open and that the Kuka Robot is in the scene." << std::endl;
+        vi.disconnect_all();
+    }
+
+    return 0;
+}
