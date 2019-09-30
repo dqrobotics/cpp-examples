3a37ee1873c4eb5e655c06f464c0bdc88415da87
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2019-09-29 22:28:59 -0700

    Example working without feedforward on the youbout

--- cmake/vrep_interface/vrep_interface_move_kuka_and_youbot.cpp
+++ cmake/vrep_interface/vrep_interface_move_kuka_and_youbot.cpp
@@ -73,7 +73,7 @@ int main(void)
         simulation_parameters.move_manipulator = true;
         simulation_parameters.wd = 0.5;
         simulation_parameters.wn = 0.1;
-        simulation_parameters.total_time = 10.0;
+        simulation_parameters.total_time = 20.0;
         simulation_parameters.dispz = 0.1;
 
         if(!vi.connect(19998,100,5))
