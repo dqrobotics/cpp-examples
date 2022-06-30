ba2ac50762ff52c9963ee9c0b781699095be6a1c
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2022-06-30 10:40:37 +0900

    [.test_qpoases.sh] Added missing $ causing the script to fail.

--- .test_qpoases.sh
+++ .test_qpoases.sh
@@ -7,7 +7,7 @@ declare -a qpoases_examples_array=("qpoases_interface")
 for qpoases_example in ${qpoases_examples_array[@]}; do
     cd $qpoases_example
     cd build
-    ./qpoases_example
+    ./$qpoases_example
     # Guaranteeing a error when the build fails.
     if [ $? -eq 0 ]
     then
