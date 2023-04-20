e42184fbc589e0fd2f140c122aa07d2dbc43958f
Murilo M. Marinho <murilomarinho@ieee.org>
2023-04-20 12:30:01 +0900

    [json11_interface] Added test for SerialManipulatorDenso

--- cmake/json11_interface/json11_interface_example.cpp
+++ cmake/json11_interface/json11_interface_example.cpp
@@ -16,24 +16,8 @@ int main(void)
     return 0;
 }
 
-static void print_kinematics_info(DQ_Kinematics* kinematics)
+static VectorXd deg2rad_with_mask(const VectorXd& v, const VectorXi& mask)
 {
-    std::cout << "	reference_frame = " << kinematics->get_reference_frame() << std::endl;
-}
-
-static void print_serialmanipulator_info(DQ_SerialManipulator* serial_manipulator)
-{
-    std::cout << "	lower_q_limit = " << serial_manipulator->get_lower_q_limit().transpose() << std::endl;
-    std::cout << "	upper_q_limit = " << serial_manipulator->get_upper_q_limit().transpose() << std::endl;
-    std::cout << "	lower_q_dot_limit = " << serial_manipulator->get_lower_q_dot_limit().transpose() << std::endl;
-    std::cout << "	upper_q_dot_limit = " << serial_manipulator->get_upper_q_dot_limit().transpose() << std::endl;
-    std::cout << "	effector = " << serial_manipulator->get_effector() << std::endl;
-}
-
-static VectorXd deg2rad_with_mask(const VectorXd& v, const VectorXi& mask=VectorXi())
-{
-    if(mask.size() == 0)
-        return v;
     if(v.size()!=mask.size())
         throw std::runtime_error("DQ_JsonReader::deg2rad_with_mask::Invalid mask size.");
 
@@ -46,59 +30,78 @@ static VectorXd deg2rad_with_mask(const VectorXd& v, const VectorXi& mask=Vector
     return v_with_mask;
 }
 
+void test_dq_serial_manipulator(DQ_SerialManipulator* dqsm, VectorXi angle_mask)
+{
+    //"lower_q_limit":[-1 ,-2 ,-3 ,-4 ,-5 ,-6 ,-7 ,-8 ,-9 ],
+    VectorXd lower_q_limit_check(9); lower_q_limit_check<<-1 ,-2 ,-3 ,-4 ,-5 ,-6 ,-7 ,-8 ,-9;
+    assert((dqsm->get_lower_q_limit()==deg2rad_with_mask(lower_q_limit_check,angle_mask)));
+    //"upper_q_limit":[9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1 ],
+    VectorXd upper_q_limit_check(9); upper_q_limit_check<<9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1;
+    assert((dqsm->get_upper_q_limit()==deg2rad_with_mask(upper_q_limit_check,angle_mask)));
+    //"lower_q_dot_limit":[-9, -8, -7, -6, -5, -4, -3, -2, -1],
+    VectorXd lower_q_dot_limit_check(9); lower_q_dot_limit_check<<-9, -8, -7, -6, -5, -4, -3, -2, -1;
+    assert((dqsm->get_lower_q_dot_limit()==deg2rad_with_mask(lower_q_dot_limit_check,angle_mask)));
+    //"upper_q_dot_limit":[1, 2, 3, 4, 5, 6, 7, 8, 9],
+    VectorXd upper_q_dot_limit_check(9); upper_q_dot_limit_check<<1, 2, 3, 4, 5, 6, 7, 8, 9;
+    assert((dqsm->get_upper_q_dot_limit()==deg2rad_with_mask(upper_q_dot_limit_check,angle_mask)));
+    //"effector":[0.0,1.0,0.0,0.0],
+    DQ effector_check = i_;
+    assert(dqsm->get_effector()==effector_check);
+    //"reference_frame":[0.0,0.0,1.0,0.0]
+    DQ reference_frame_check = j_;
+    assert(dqsm->get_reference_frame()==reference_frame_check);
+}
+
 void load_dq_serial_manipulator_dh()
 {
+    std::cout << "Loading DQ_SerialManipulatorDH..." << std::endl;
+
     DQ_SerialManipulatorDH robot_dh = DQ_JsonReader::get_from_json<DQ_SerialManipulatorDH>("../dq_serial_manipulator_dh.json");
 
-//"theta":[-90, 90, -90, 0, 90, -90,  0,  0,  0],
+    //"theta":[-90, 90, -90, 0, 90, -90,  0,  0,  0],
     VectorXd thetas_check(9); thetas_check << -90, 90, -90, 0, 90, -90,  0,  0,  0;
     assert(robot_dh.get_thetas()==deg2rad(thetas_check));
-//"d":[0.345, 0, 0, 0.255, 0, 0, 0.1198375,  0.27, 0],
+    //"d":[0.345, 0, 0, 0.255, 0, 0, 0.1198375,  0.27, 0],
     VectorXd d_check(9); d_check << 0.345, 0, 0, 0.255, 0, 0, 0.1198375,  0.27, 0;
     assert(robot_dh.get_ds()==d_check);
-//"a":[0.0, 0.255, 0.01, 0, 0, 0, 0, 0, 0],
+    //"a":[0.0, 0.255, 0.01, 0, 0, 0, 0, 0, 0],
     VectorXd a_check(9); a_check << 0.0, 0.255, 0.01, 0, 0, 0, 0, 0, 0;
     assert(robot_dh.get_as()==a_check);
-//"alpha":[90, 0, -90, 90, 90, 0, -90, 90, 0],
+    //"alpha":[90, 0, -90, 90, 90, 0, -90, 90, 0],
     VectorXd alpha_check(9); alpha_check << 90, 0, -90, 90, 90, 0, -90, 90, 0;
     assert(robot_dh.get_alphas()==deg2rad(alpha_check));
-//"types":[0, 0, 1, 0, 0, 1, 0, 0, 0],
+    //"types":[0, 0, 1, 0, 0, 1, 0, 0, 0],
     VectorXd types_check(9); types_check << 0, 0, 1, 0, 0, 1, 0, 0, 0;
     assert(robot_dh.get_types()==types_check);
-//"lower_q_limit":[-1 ,-2 ,-3 ,-4 ,-5 ,-6 ,-7 ,-8 ,-9 ],
-    VectorXd lower_q_limit_check(9); lower_q_limit_check<<-1 ,-2 ,-3 ,-4 ,-5 ,-6 ,-7 ,-8 ,-9;
-    assert((robot_dh.get_lower_q_limit()==deg2rad_with_mask(lower_q_limit_check,types_check.cast<int>())));
-//"upper_q_limit":[9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1 ],
-    VectorXd upper_q_limit_check(9); upper_q_limit_check<<9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1;
-    assert((robot_dh.get_upper_q_limit()==deg2rad_with_mask(upper_q_limit_check,types_check.cast<int>())));
-//"lower_q_dot_limit":[-9, -8, -7, -6, -5, -4, -3, -2, -1],
-    VectorXd lower_q_dot_limit_check(9); lower_q_dot_limit_check<<-9, -8, -7, -6, -5, -4, -3, -2, -1;
-    assert((robot_dh.get_lower_q_dot_limit()==deg2rad_with_mask(lower_q_dot_limit_check,types_check.cast<int>())));
-//"upper_q_dot_limit":[1, 2, 3, 4, 5, 6, 7, 8, 9],
-    VectorXd upper_q_dot_limit_check(9); upper_q_dot_limit_check<<1, 2, 3, 4, 5, 6, 7, 8, 9;
-    assert((robot_dh.get_upper_q_dot_limit()==deg2rad_with_mask(upper_q_dot_limit_check,types_check.cast<int>())));
-//"effector":[0.0,1.0,0.0,0.0],
-    DQ effector_check = i_;
-    assert(robot_dh.get_effector()==effector_check);
-//"reference_frame":[0.0,0.0,1.0,0.0]
-    DQ reference_frame_check = j_;
-    assert(robot_dh.get_reference_frame()==reference_frame_check);
+
+    test_dq_serial_manipulator(static_cast<DQ_SerialManipulator*>(&robot_dh), types_check.cast<int>());
 
     std::cout << "Loaded and checked DQ_SerialManipulatorDH." << std::endl;
 }
 
 void load_dq_serial_manipulator_denso()
 {
+    std::cout << "Loading DQ_SerialManipulatorDenso..." << std::endl;
+
     DQ_SerialManipulatorDenso robot_denso = DQ_JsonReader::get_from_json<DQ_SerialManipulatorDenso>("../dq_serial_manipulator_denso.json");
 
-    std::cout << "Loaded DQ_SerialManipulatorDH with" << std::endl;
-    std::cout << "	alphas = " << robot_denso.get_alphas().transpose() << std::endl;
-    std::cout << "	betas = " << robot_denso.get_betas().transpose() << std::endl;
-    std::cout << "	thetas = " << robot_denso.get_gammas().transpose() << std::endl;
-    std::cout << "	as = " << robot_denso.get_as().transpose() << std::endl;
-    std::cout << "	bs = " << robot_denso.get_bs().transpose() << std::endl;
-    std::cout << "	ds = " << robot_denso.get_ds().transpose() << std::endl;
+    //"a":    [0,     0,      -0.009935, 0.000182,  -0.00002,  0],
+    VectorXd a_check(6); a_check << 0,     0,      -0.009935, 0.000182,  -0.00002,  0;
+    //"b":    [0,     -0.2501, 0,      0,      0,      0],
+    VectorXd b_check(6); b_check << 0,     -0.2501, 0,      0,      0,      0;
+    //"d":    [0.345,   0,      0,      0.25509, -0.00017,  0.069967],
+    VectorXd d_check(6); d_check << 0.345,   0,      0,      0.25509, -0.00017,  0.069967;
+    //"alpha":[-89.94,0.039,  90.018, -89.95, 89.986, 0],
+    VectorXd alpha_check(6); alpha_check << -89.94,0.039,  90.018, -89.95, 89.986, 0;
+    assert(robot_denso.get_alphas()==deg2rad(alpha_check));
+    //"beta": [0,     -0.009, 0,      0,      0,      0],
+    VectorXd beta_check(6); beta_check << 0,     -0.009, 0,      0,      0,      0;
+    assert(robot_denso.get_betas()==deg2rad(beta_check));
+    //"gamma":[0,     0.017,  -0.015, -0.115, -0.013, 0],
+    VectorXd gamma_check(6); gamma_check << 0,     0.017,  -0.015, -0.115, -0.013, 0;
+    assert(robot_denso.get_gammas()==deg2rad(gamma_check));
 
-    print_kinematics_info(static_cast<DQ_Kinematics*>(&robot_denso));
-    print_serialmanipulator_info(static_cast<DQ_SerialManipulator*>(&robot_denso));
+    test_dq_serial_manipulator(static_cast<DQ_SerialManipulator*>(&robot_denso), VectorXi::Zero(6));
+
+    std::cout << "Loaded and checked DQ_SerialManipulatorDenso." << std::endl;
 }
